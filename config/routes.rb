a932d0f2bc6570edca796c85e224f65a8a32bc83
Raghu Betina <rbetina@github.com>
2015-04-28 18:59:46 -0500

    Adds routes for movies, completes Index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,16 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Routes to CREATE movies
+  get("/movies/new",       { :controller => "movies", :action => "new_form" })
+  get("/create_movie",     { :controller => "movies", :action => "create_row" })
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Routes to READ movies
+  get("/movies",           { :controller => "movies", :action => "index" })
+  get("/movies/:id",       { :controller => "movies", :action => "show" })
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Routes to UPDATE movies
+  get("/movies/:id/edit",  { :controller => "movies", :action => "edit_form" })
+  get("/update_movie/:id", { :controller => "movies", :action => "update_row" })
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # Route to DESTROY movies
+  get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
 end
