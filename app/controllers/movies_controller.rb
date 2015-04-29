0f6aae940dc06974543eab77dea6d86817085343
Raghu Betina <rbetina@github.com>
2015-04-28 19:23:42 -0500

    Completes Create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,25 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.order("created_at DESC")
   end
 
   def show
     @movie = Movie.find(params[:zebra])
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    m = Movie.new
+    m.title = params[:the_title]
+    m.year = params[:the_year]
+    m.duration = params[:the_duration]
+    m.description = params[:the_description]
+    m.image_url = params[:the_image_url]
+    m.save
+
+    redirect_to("/movies")
   end
 end
