03f09c5fb108fbb99a71d3062234add5a2c7ee22
Raghu Betina <rbetina@github.com>
2015-04-28 19:52:49 -0500

    Slight re-organization; no functional effect

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,30 @@
 class MoviesController < ApplicationController
+  def new_form
+  end
+
+  def create_row
+    m = Movie.new
+
+    m.title = params[:the_title]
+    m.year = params[:the_year]
+    m.duration = params[:the_duration]
+    m.description = params[:the_description]
+    m.image_url = params[:the_image_url]
+    m.save
+
+    redirect_to("/movies")
+  end
+
   def index
     @movies = Movie.order("created_at DESC")
   end
 
   def show
     @movie = Movie.find(params[:zebra])
+  end
+
+  def edit_form
+    @movie = Movie.find(params[:id])
   end
 
   def update_row
@@ -20,42 +40,11 @@ class MoviesController < ApplicationController
     redirect_to("/movies/#{m.id}")
   end
 
-  def new_form
-  end
-
-  def create_row
-    m = Movie.new
-    m.title = params[:the_title]
-    m.year = params[:the_year]
-    m.duration = params[:the_duration]
-    m.description = params[:the_description]
-    m.image_url = params[:the_image_url]
-    m.save
-
-    redirect_to("/movies")
-  end
-
-  def edit_form
-    @movie = Movie.find(params[:id])
-  end
-
   def destroy
     m = Movie.find(params[:id])
+
     m.destroy
 
     redirect_to("/movies")
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
