1905c33d1c8121bd0b960f67397dc31a33328976
Raghu Betina <rbetina@github.com>
2015-05-05 17:43:37 -0500

    Got rid of everything. Almost a blank app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-  # Routes to CREATE movies
-  get("/movies/new",       { :controller => "movies", :action => "new_form" })
-  get("/create_movie",     { :controller => "movies", :action => "create_row" })
 
-  # Routes to READ movies
-  get("/movies",           { :controller => "movies", :action => "index" })
-  get("/movies/:zebra",       { :controller => "movies", :action => "show" })
-
-  # Routes to UPDATE movies
-  get("/movies/:id/edit",  { :controller => "movies", :action => "edit_form" })
-  get("/update_movie/:id", { :controller => "movies", :action => "update_row" })
-
-  # Route to DESTROY movies
-  get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
 end
